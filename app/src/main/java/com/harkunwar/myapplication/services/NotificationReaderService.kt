af1f9e48312920dcfde7b63f7b1e448eb12ab59a
Harkunwar <geek@harkunwar.com>
2018-11-04 22:08:54 -0800

    implement speaking

--- app/src/main/java/com/harkunwar/myapplication/services/NotificationReaderService.kt
+++ app/src/main/java/com/harkunwar/myapplication/services/NotificationReaderService.kt
@@ -6,13 +6,16 @@ import android.accessibilityservice.AccessibilityServiceInfo
 import android.app.Notification
 import android.util.Log
 import android.app.PendingIntent.CanceledException
-
-
+import android.os.Build
+import java.util.logging.Logger
+import android.speech.tts.TextToSpeech
+import java.util.*
 
 
 class NotificationReaderService : AccessibilityService() {
     
     val TAG = "WhatsappAccessExample"
+    lateinit var t1: TextToSpeech
 
     override fun onServiceConnected() {
         Log.d(TAG, "AccessibilityService Connected")
@@ -33,28 +36,60 @@ class NotificationReaderService : AccessibilityService() {
         if (event != null) {
             @Suppress("DEPRECATED_IDENTITY_EQUALS")
             if (event.eventType === AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
-                Log.d(TAG, "Recieved event")
-                val data = event.parcelableData
-                if (data is Notification) {
-                    Log.d(TAG, "Recieved notification")
-                    Log.d(TAG, "ticker: " + data.tickerText)
-                    Log.d(TAG, "icon: " + data.iconLevel)
-                    Log.d(TAG, "notification: " + event.text)
-                    Log.d(TAG, "pkg name: "+event.packageName)
-                    Log.d(TAG, "before text: "+event.beforeText)
+                if(event.packageName.equals("com.whatsapp")) {
+                    Log.d(TAG, "Recieved event")
+                    val data = event.parcelableData
+                    if (data is Notification) {
+                        Log.d(TAG, "Recieved notification")
+                        Log.d(TAG, "ticker: " + data.tickerText)
+                        Log.d(TAG, "icon: " + data.iconLevel)
+                        Log.d(TAG, "notification: " + event.text)
+                        Log.d(TAG, "pkg name: " + event.packageName)
+                        Log.d(TAG, "before text: " + event.beforeText)
 
-                    try {
-                        if(event.packageName == "com.whatsapp")
-                            data.contentIntent.send()
 
-                    } catch (e1: CanceledException) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace()
+                        var latestMessage = ""
+                        val lines = data.extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES)
+                        if (lines != null) {
+                            latestMessage = lines[lines.size - 1] as String
+                        } else {
+
+    //                        val msgs = ArrayList<String>()
+    //                        msgs.add("Info: " + data.extras.getString(Notification.EXTRA_INFO_TEXT))
+    //                        msgs.add("Title: " + data.extras.getString(Notification.EXTRA_TITLE))
+                            latestMessage = data.extras.getString(Notification.EXTRA_TEXT) as String
+                        }
+
+                        Log.d(TAG, "MESSAGE: $latestMessage")
+
+                        t1 = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
+                            if (status != TextToSpeech.ERROR) {
+                                t1.language = Locale.UK
+                                speakOut(latestMessage)
+                            }
+                        })
+    //
+    //
+    //                    try {
+    //                        if(event.packageName == "com.whatsapp")
+    //                            data.contentIntent.send()
+    //
+    //
+    //                    } catch (e1: CanceledException) {
+    //                        e1.printStackTrace()
+    //                    }
                     }
-
                 }
             }
         }
     }
 
+    private fun speakOut(message: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            t1.speak(message, TextToSpeech.QUEUE_FLUSH, null,"")
+        }
+        else {
+            t1.speak(message, TextToSpeech.QUEUE_FLUSH, null)
+        }
+    }
 }
